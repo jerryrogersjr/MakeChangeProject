3d9facf2f5367f441f1a810a92f9bd55c5311298
Jerry Rogers <jerryrogersjr@gmail.com>
2019-09-14 19:15:42 -0600

    cleaning up code a bit

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 /* This app is designed to calculate change to return to customer as well as remaining
  * balance if needed. There is a lot that can be done to advance this program, but with time.
- * 
+ * ...and a bit more learning :)
  */
 
 public class MakeChangeApp {
@@ -18,43 +18,40 @@ public class MakeChangeApp {
 		DecimalFormat df = new DecimalFormat("#,###.00"); // formats my decimal to two places and allows for commas
 
 		System.out.print("Enter your payment amount: $ "); // renders payment amount
-		double payment = kb.nextDouble(); 
+		double payment = kb.nextDouble();
 		System.out.println();
-		kb.close(); 
+		kb.close();
 
 		double changeDue = payment - total; // calculates difference
-		double change = changeDue * 100; 
+		double change = changeDue * 100;
 
-		change = change / 100;
+		int bill50 = (int) (change / 5000); // $50 bill
+		change = change % 5000;
+
+		int bill20 = (int) (change / 2000); // $20 bill
+		change = change % 2000;
+
+		int bill10 = (int) (change / 1000); // $10 bill
+		change = change % 1000;
+
+		int bill5 = (int) (change / 500); // $5 bill
+		change = change % 500;
+
+		int bill1 = (int) (change / 100); // $1 bill
 		change = change % 100;
 
-		int bill50 = (int) (change / 50); // $50 bill
-		change = change % 50;
+		int quarters = (int) (change / 25); // $0.25 cents
+		change = change % 25;
 
-		int bill20 = (int) (change / 20); // $20 bill
-		change = change % 20;
-
-		int bill10 = (int) (change / 10); // $10 bill
+		int dimes = (int) (change / 10); // $0.10 cents
 		change = change % 10;
 
-		int bill5 = (int) (change / 5); // $5 bill
+		int nickels = (int) (change / 5); // $0.05 cents
 		change = change % 5;
 
-		int bill1 = (int) (change / 1); // $1 bill
-		change = change % 1;
-
-		int quarters = (int) (change / .25); // $0.25 cents
-		change = change % .25;
-
-		int dimes = (int) (change / .10); // $0.10 cents
-		change = change % .10;
-
-		int nickels = (int) (change / .05); // $0.05 cents
-		change = change % .05;
-
 		// System.out.println("This is a temporary line to check change " + change);
-		int pennies = (int) ((change / .01) + 0.001); // $0.01 cents
-		change = change % .01;
+		int pennies = (int) ((change / 1) + 0.001); // $0.01 cents
+		change = change % 1;
 
 		if (changeDue > 0) {
 			System.out.println("Thank you for your payment!");
@@ -76,17 +73,15 @@ public class MakeChangeApp {
 		} else if (changeDue == 0) {
 			System.out.println("Exact Payment...No Change Due");
 			System.out.println("Thank you for your payment!");
+
 		} else if (changeDue < 0) {
 			double totalRemaining = changeDue;
 			System.err.println("ERROR...Not enough money. Please provide remaing change due");
 			System.out.println();
 			System.out.println("**********************************************************\n");
 			System.out.println("Money still owed: $ " + df.format(Math.abs(totalRemaining)));
-
 		}
-
 	}
-
 	public static void welcome() {
 		System.out.println("**********************************************************\n");
 		System.out.println("                         Hello!           \n");
